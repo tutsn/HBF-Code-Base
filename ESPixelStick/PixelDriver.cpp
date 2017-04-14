493364b1fadc6ecfec8c5cb05ec4a7eaee274172
Gabriel le Roux <leroux@zib.de>
2017-04-14 16:45:42 +0200

    Messy output removed

--- ESPixelStick/PixelDriver.cpp
+++ ESPixelStick/PixelDriver.cpp
@@ -307,8 +307,8 @@ void PixelDriver::sendPeriDMX(){
     Wire.beginTransmission(8); // transmit to device #8
     Wire.write(peri_dimmer);              // sends one byte
     Wire.write(peri_dimmer);              // sends one byte
-    LOG_PORT.print("Sent Peri ");
-    LOG_PORT.println(peri_dimmer);
+    // LOG_PORT.print("Sent Peri ");
+    // LOG_PORT.println(peri_dimmer);
     Wire.endTransmission();    // stop transmitting
 }
 
